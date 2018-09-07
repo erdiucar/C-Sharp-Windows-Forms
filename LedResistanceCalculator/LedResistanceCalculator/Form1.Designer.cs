0e71008402eae4387f05ea2175a70d8e61f7f016
Erdi Uçar <ucarerdi@gmail.com>
2018-09-07 20:04:51 +0300

    Initial commit

---
+++ LedResistanceCalculator/LedResistanceCalculator/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace LedResistanceCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.beslemeGerilimText = new System.Windows.Forms.TextBox();
+            this.ledGerilimText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ledAkimText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "mA";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // beslemeGerilimText
+            // 
+            this.beslemeGerilimText.Location = new System.Drawing.Point(59, 73);
+            this.beslemeGerilimText.Name = "beslemeGerilimText";
+            this.beslemeGerilimText.Size = new System.Drawing.Size(100, 20);
+            this.beslemeGerilimText.TabIndex = 8;
+            // 
+            // ledGerilimText
+            // 
+            this.ledGerilimText.Location = new System.Drawing.Point(59, 113);
+            this.ledGerilimText.Name = "ledGerilimText";
+            this.ledGerilimText.Size = new System.Drawing.Size(100, 20);
+            this.ledGerilimText.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Besleme Gerilimi - V";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Led Gerilimi - V";
+            // 
+            // ledAkimText
+            // 
+            this.ledAkimText.Location = new System.Drawing.Point(59, 153);
+            this.ledAkimText.Name = "ledAkimText";
+            this.ledAkimText.Size = new System.Drawing.Size(100, 20);
+            this.ledAkimText.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Led Akımı - I";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 320);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.beslemeGerilimText);
+            this.Controls.Add(this.ledGerilimText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ledAkimText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox beslemeGerilimText;
+        private System.Windows.Forms.TextBox ledGerilimText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ledAkimText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LedResistanceCalculator/LedResistanceCalculator/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LedResistanceCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float ledGerilim = float.Parse(ledGerilimText.Text);
+            float beslemeGerilim = float.Parse(beslemeGerilimText.Text);
+            float ledAkim = float.Parse(ledAkimText.Text) / 1000;
+
+            float direncSonuc = ((beslemeGerilim - ledGerilim) / ledAkim);
+
+            label3.Text = direncSonuc.ToString() + " Ohm'dan yüksek en yakın direnci devreye bağlayın";
+        }
+    }
+}
